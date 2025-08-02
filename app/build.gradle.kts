a96b97a7d7dac2b6f7c8c456b3ea81a19a293057
sergcam <sergiocam43@gmail.com>
2025-08-02 13:17:18 -0700

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "dev.secam.checkin24"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-alpha"
+        versionCode = 2
+        versionName = "0.2.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
